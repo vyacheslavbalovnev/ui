83cfcb7f022ff0ade6a8b8648a60b4cf39965061
vyacheslavbalovnev <vbalovnev@mail.ru>
2018-03-06 21:49:31 +0600

    ui new

--- assembly/src/main/resources/static/controller.js
+++ assembly/src/main/resources/static/controller.js
@@ -1,6 +1,6 @@
 angular.module('controller', [])
 .controller('Controller', function($scope, $http) {
-
+    $scope.list = [];
     $scope.color = {
         name: 'blue'
       };
@@ -9,4 +9,11 @@ angular.module('controller', [])
             $scope.greeting = response.data;
 
         });
+    $scope.select = function() {
+     $http.get('/select?query=' + $scope.color.name).
+            then(function(response) {
+                $scope.list = response.data;
+
+            });
+    };
 });
